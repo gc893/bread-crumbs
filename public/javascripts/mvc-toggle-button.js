e77f86d542e36372b05cbc0be53bef31de7c001f
Gabriela Cabrera <gaby.cabrera9@gmail.com>
2020-08-29 10:30:18 -0400

    add route mapping table

--- public/javascripts/mvc-toggle-button.js
+++ public/javascripts/mvc-toggle-button.js
@@ -1,12 +1,24 @@
 const mvcBtn = document.getElementById('mvc-toggle-btn');
 const mvcDiv = document.getElementById('mvc-model-container')
 
+const mappingTableBtn = document.getElementById('routes-toggle-btn');
+const mappingTableDiv = document.getElementById('routes-table-container')
+
 mvcBtn.addEventListener('click', showMvcDiagram);
+mappingTableBtn.addEventListener('click', showRoutesTable);
 
 function showMvcDiagram() {
     if(mvcDiv.hidden){
         mvcDiv.removeAttribute('hidden');
     } else {
         mvcDiv.hidden = 'true';
+    }
+}
+
+function showRoutesTable() {
+    if(mappingTableDiv.hidden){
+        mappingTableDiv.removeAttribute('hidden');
+    } else {
+        mappingTableDiv.hidden = 'true';
     }
 }
