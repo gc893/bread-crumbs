7c61f9f918d8f5117d10e881559da6bb3fc94338
Gabriela Cabrera <gaby.cabrera9@gmail.com>
2020-08-30 12:51:45 -0400

    finalized index function

--- controllers/guides.js
+++ controllers/guides.js
@@ -1,7 +1,17 @@
 const Guide = require('../models/guides');
 
 function index(req, res) {
-  res.render('guides/index', {title: 'Guides', user: req.user})
+  Guide.find({}, 'action app_component', function(err, guides) {
+    const actions = [];
+    guides.forEach(el => {
+      if (!actions.find(target => target === el.action)) {actions.push(el.action)}
+    })
+    const components = [];
+    guides.forEach(el => {
+      if (!components.find(target => target === el.app_component)) {components.push(el.app_component)}
+    })
+    res.render('guides/index', {title: 'Guides', user: req.user, actions, components});
+  })
   }
 
 function newGuide(req, res) {
