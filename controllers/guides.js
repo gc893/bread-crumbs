dd7c2ff8259e37b717077acc547cc45a908e50fb
Gabriela Cabrera <gaby.cabrera9@gmail.com>
2020-08-30 13:07:30 -0400

    fix index function

--- controllers/guides.js
+++ controllers/guides.js
@@ -1,7 +1,8 @@
 const Guide = require('../models/guides');
 
 function index(req, res) {
-  Guide.find({}, 'action app_component', function(err, guides) {
+  Guide.find({}, null, function(err, guides) {
+    console.log(guides);
     const actions = [];
     guides.forEach(el => {
       if (!actions.find(target => target === el.action)) {actions.push(el.action)}
@@ -12,29 +13,29 @@ function index(req, res) {
     })
     res.render('guides/index', {title: 'Guides', user: req.user, actions, components});
   })
-  }
+}
 
 function newGuide(req, res) {
   res.render('guides/new', {title: 'Enter Title', user: req.user})
-  }
+}
 
-  function create(req, res) {
-    for (let key in req.body) {
-      if (req.body[key] === '') delete req.body[key];
-    }
-    Guide.create(req.body)
-    .then(guide => {
-        console.log(guide);
-        res.redirect('/guides');//should redirect to show to add steps
-    })
-    .catch( err => {
-        console.log(err);
-        res.redirect('/guides');
-    })
+function create(req, res) {
+  for (let key in req.body) {
+    if (req.body[key] === '') delete req.body[key];
   }
+  Guide.create(req.body)
+  .then(guide => {
+      console.log(guide);
+      res.redirect('/guides');//should redirect to show to add steps
+  })
+  .catch( err => {
+      console.log(err);
+      res.redirect('/guides');
+  })
+}
 
-  module.exports = {
-    index,
-    new: newGuide,
-    create
+module.exports = {
+  index,
+  new: newGuide,
+  create
 };
