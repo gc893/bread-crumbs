b35d7acc1d46a6fc53ae81a994178f45633b0c86
Gabriela Cabrera <gaby.cabrera9@gmail.com>
2020-09-02 11:04:14 -0400

    restrict routes

--- routes/guides.js
+++ routes/guides.js
@@ -5,19 +5,28 @@ var guidesCtrl = require('../controllers/guides');
 router.get('/', isLoggedIn, guidesCtrl.index);//index
 router.get('/search', isLoggedIn, guidesCtrl.search);//use search middleware
 router.get('/list', isLoggedIn, guidesCtrl.list);
-router.get('/new', isLoggedIn, guidesCtrl.new);//render new view//admin
-router.get('/editStep', isLoggedIn, guidesCtrl.editStep);//render edit view//admin
+router.get('/new', isAdmin, guidesCtrl.new);//render new view//admin
+router.get('/editStep', isAdmin, guidesCtrl.editStep);//render edit view//admin
 router.get('/:id', isLoggedIn, guidesCtrl.show);//show guide, comments and add comment box
-router.get('/:id/edit', isLoggedIn, guidesCtrl.editGuide);//render new view and load guide//admin
-router.post('/', isLoggedIn, guidesCtrl.create);//add new guide//admin
-router.post('/:id/steps', isLoggedIn, guidesCtrl.addStep);//add new step//admin
+router.get('/:id/edit', isAdmin, guidesCtrl.editGuide);//render new view and load guide//admin
+router.post('/', isAdmin, guidesCtrl.create);//add new guide//admin
+router.post('/:id/steps', isAdmin, guidesCtrl.addStep);//add new step//admin
 router.post('/:id/comments', isLoggedIn ,guidesCtrl.addComment);//add new comment
-router.put('/:id', isLoggedIn, guidesCtrl.updateGuide);
-router.put('/:stepId/updateStep', isLoggedIn, guidesCtrl.updateStep);//edit step//admin
-router.delete('/:id/:stepId', isLoggedIn, guidesCtrl.removeStep);
+router.put('/:id', isAdmin, guidesCtrl.updateGuide);
+router.put('/:stepId/updateStep', isAdmin, guidesCtrl.updateStep);//edit step//admin
+router.delete('/:id/:stepId', isAdmin, guidesCtrl.removeStep);
 
 function isLoggedIn(req, res, next) {
     if (req.isAuthenticated()) return next();
+    res.redirect("/");
+  }
+
+  function isAdmin(req, res, next) {
+    if (req.isAuthenticated()){
+      if(req.user.role ==='admin'){
+        return next();
+      }
+    }
     res.redirect("/");
   }
 
